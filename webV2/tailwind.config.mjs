4d3dc81a0b306df7e7a85eac9313349eea04e209
roomNdev <romndev@gmail.com>
2024-11-21 19:17:14 -0300

    Fixes for mobile version

--- webV2/tailwind.config.mjs
+++ webV2/tailwind.config.mjs
@@ -2,15 +2,26 @@
 const safelist = [
 	// ...['group-hover/child:flex', 'group-hover/child:opacity-100', 'group/child'],
 	// ...['group-hover/parent:flex', 'group-hover/parent:opacity-100', 'group/parent'],
-	...[...[1,2,3,4,5,6,7,8].map(n => `grid-cols-${n}`)],
-	'mb-64'
+	{
+		pattern: /grid-cols-./,
+		variants: ['sm', 'md', 'lg', 'xl', '2xl', 'xs'],
+	  },
+	// ...[...[1,2,3,4,5,6,7,8].map(n => `grid-cols-${n}`)],
+	// ...[...[1,2,3,4,5,6,7,8].map(n => `lg:grid-cols-${n}`)],
+	// ...[...[1,2,3,4,5,6,7,8].map(n => `xs:grid-cols-${n}`)],
+	'mb-64',
   ]
 
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'sm': '480px',
+				// => @media (min-width: 992px) { ... }
+			  },
+		},
 	},
 	safelist,
 	plugins: [],
